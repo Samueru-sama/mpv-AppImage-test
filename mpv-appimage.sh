2082a7f0b17055fbf926e3a75627a212baf77cdf
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-23 16:25:24 -0400

    oops

--- mpv-appimage.sh
+++ mpv-appimage.sh
@@ -32,7 +32,7 @@ wget "$LIB4BN" -O ./lib4bin
 chmod +x ./lib4bin
 ./lib4bin -p -v -s -k -e \
 	"$(command -v mpv)" \
-	/usr/lib/libGLX*
+	/usr/lib/libEGL*
 
 VERSION=$(./bin/mpv --version | awk 'FNR==1 {print $2; exit}')
 if [ -z "$VERSION" ]; then
